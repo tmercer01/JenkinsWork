98aeda67f30736a25583987dc203c93350e25656
Tim <hexstu002@sdettraining.com>
2016-04-05 10:14:57 -0400

    Hello Java file created

--- Hello.java
+++ Hello.java
@@ -2,6 +2,11 @@ package com.naveen;
 
 class Hello{
 	public void sayHi(){
-		System.out.println("hi");
+		System.out.println("h from git to jenkinsi");
 	}
+
+	public static void main(String args[]){
+		new Hello().sayHi();
+	}
+
 }
